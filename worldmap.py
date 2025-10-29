25edcf5e6aac20e400340e7fff28ac50c666c2f8
Dawson <126727190+DawMC@users.noreply.github.com>
2025-10-29 22:41:18 +0000

    fixed the code and switched it over to a more zoom ed-in view.

--- worldmap.py
+++ worldmap.py
@@ -1,11 +1,11 @@
 import folium
 import pandas as pd
 
-m = folium.Map(location=[36, -95], zoom_start=10)
+m = folium.Map(location=[36, -95], zoom_start=.25)
 
 fg_places = folium.FeatureGroup(name = "Places")
 
-folium.Marker(location= [39.8283, -98.5795], popup="US", icon=folium.i|con.Icon(color='blue')).add_to(fg_places)
+folium.Marker(location= [39.8283, -98.5795], popup="US", icon=folium.Icon(color='blue')).add_to(fg_places)
 folium.Marker(location= [30.5595, -22.9375], popup ="South Africa", icon = folium.Icon(color='green')).add_to(fg_places)
 folium.Marker(location= [36.2048, 138.2529], popup="Japan", icon=folium.Icon(color='green')).add_to(fg_places)
 folium.Marker(location= [54.5260, 15.2551], popup="Europe", icon=folium.Icon(color='red')).add_to(fg_places)
@@ -38,3 +38,8 @@ places = [
         "Compared to USD": "1 USD = 5.25 BRL",
     }
 ]
+
+fg_places.add_to(m)
+folium.LayerControl(collapsed=False).add_to(m)
+
+m
